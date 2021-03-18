67cc26c82107fa5bd03632596466a20452145a8d
이주이 <dea8307@naver.com>
2021-03-18 17:37:58 +0900

    feat: mypage_diary, reviewdetail

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import MainPage from './components/views/MainPage/MainPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import MyPage from './components/views/MyPage/MyPage';
+import FollowPage from './components/views/FollowPage/FollowPage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
             <Route exact path="/" component={MainPage} />
             <Route exact path="/login" component={LoginPage} />
             <Route path="/register" component={RegisterPage} />
+            <Route path="/mypage" component={MyPage} />
+            <Route path="/follow" component={FollowPage} />
           </Switch>
         </div>
       </Router>
