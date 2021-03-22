e3db96f56a660aa6eaa1ddf5cd10e609b10e7dd6
zzdhxm12@naver.com <zzdhxm12@naver.com>
2021-03-22 14:20:50 +0900

    django 가게상세 구현 중

--- server/stores/views.py
+++ server/stores/views.py
@@ -1,2 +1,20 @@
 from django.shortcuts import render
+
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import StoreSerializer, MenuSerializer
+from .models import Store, Menu
+
+
 # Create your views here.
+#가게상세정보 불러오기
+@api_view(['GET'])
+def store_detail(request, id):
+    store = Store.objects.get(id=id)
+
+    serializer = StoreSerializer(store)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
