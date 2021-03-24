c8661dc784d282b84a928feb3d31ad36ba0403f9
이주이 <dea8307@naver.com>
2021-03-24 15:25:56 +0900

    S04P23D107-78 feat: store detail mapapi 적용시키기

--- client/src/components/views/StoreDetailPage/Sections/StoreMap.js
+++ client/src/components/views/StoreDetailPage/Sections/StoreMap.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import MapContainer from './MapContainer';
 
 function StoreMap() {
   return (
-    <div>
-      <img src={'https://picsum.photos/280/400/?random'} />
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '0.5rem',
+        marginBottom: '0.5rem',
+      }}
+    >
+      {/* <img src={'https://picsum.photos/280/400/?random'} /> */}
+      <MapContainer />
     </div>
   );
 }
