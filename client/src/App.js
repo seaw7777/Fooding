32efb209421f692a38c9ad88db899f3da192860a
이주이 <dea8307@naver.com>
2021-03-25 22:00:53 +0900

    feat: mypage_review_넣기

--- client/src/App.js
+++ client/src/App.js
@@ -20,39 +20,49 @@ function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
-        <div style={{ paddingTop: '0', minHeight: 'calc(98vh - 45px)' }}>
-          <NavBarPage />
-          <Route exact path="/" component={Auth(MainPage, true)} />
-          {/* 로그인, 회원가입 하단바가 안보여야함 */}
+        <>
+          <div style={{ paddingTop: '0', minHeight: 'calc(98vh - 45px)' }}>
+            <NavBarPage />
+            <Route exact path="/" component={Auth(MainPage, true)} />
+            {/* 로그인, 회원가입 하단바가 안보여야함 */}
 
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route
-            exact
-            path="/register/taste"
-            component={Auth(RegisterTastePage, false)}
-          />
-          <Route exact path="/mypage" component={Auth(Mypage, true)} />
-          <Route exact path="/follow" component={Auth(FollowPage, true)} />
-          <Route exact path="/mypage/spoon" component={Auth(SpoonPage, true)} />
-          <Route
-            exact
-            path="/mypage/update"
-            component={Auth(UserUpdatePage, true)}
-          />
-          <Route
-            exact
-            path="/store/:StoreId"
-            component={Auth(StoreDetailPage, true)}
-          />
-          <Route
-            exact
-            path="/accompany"
-            component={Auth(AccompanyPage, true)}
-          />
-          <Route exact path="/search" component={Auth(SearchPage, true)} />
-        </div>
-        <Footer />
+            <Route exact path="/login" component={Auth(LoginPage, false)} />
+            <Route
+              exact
+              path="/register"
+              component={Auth(RegisterPage, false)}
+            />
+            <Route
+              exact
+              path="/register/taste"
+              component={Auth(RegisterTastePage, false)}
+            />
+            <Route exact path="/mypage" component={Auth(Mypage, true)} />
+            <Route exact path="/follow" component={Auth(FollowPage, true)} />
+            <Route
+              exact
+              path="/mypage/spoon"
+              component={Auth(SpoonPage, true)}
+            />
+            <Route
+              exact
+              path="/mypage/update"
+              component={Auth(UserUpdatePage, true)}
+            />
+            <Route
+              exact
+              path="/store/:StoreId"
+              component={Auth(StoreDetailPage, true)}
+            />
+            <Route
+              exact
+              path="/accompany"
+              component={Auth(AccompanyPage, true)}
+            />
+            <Route exact path="/search" component={Auth(SearchPage, true)} />
+          </div>
+          <Footer />
+        </>
       </BrowserRouter>
     </Suspense>
   );
