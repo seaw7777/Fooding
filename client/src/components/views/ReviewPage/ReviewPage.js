402bd4dd9df5188e1eef7009c91bbeadba761e14
정예림 <jinju151002@gmail.com>
2021-03-29 22:23:00 +0900

    feat: add review rating

--- client/src/components/views/ReviewPage/ReviewPage.js
+++ client/src/components/views/ReviewPage/ReviewPage.js
@@ -1,15 +1,32 @@
 import React from 'react';
-
+import Rating from '@material-ui/lab/Rating';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import { Input } from 'antd';
-// import ImageUploader from 'react-images-upload';
+import ImageUploader from 'react-images-upload';
 
 function ReviewPage() {
   const { TextArea } = Input;
+  const [value, setValue] = React.useState(0);
 
   return (
     <div>
-      <div>
-        <h1>별점 매기는 곳</h1>
+      <div style={{ display: 'flex', margin: '2rem auto' }}>
+        <Box
+          component="fieldset"
+          mb={3}
+          borderColor="transparent"
+          style={{ display: 'flex', margin: ' auto' }}
+        >
+          <Rating
+            name="simple-controlled"
+            value={value}
+            size="large"
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          />
+        </Box>
       </div>
       <div style={{ width: '90%', margin: '3rem auto' }}>
         <TextArea
@@ -20,13 +37,13 @@ function ReviewPage() {
         />
       </div>
       <div>
-        {/* <ImageUploader
+        <ImageUploader
           withIcon={true}
           buttonText="사진 업로드"
           // onChange={this.onDrop}
           imgExtension={['.jpg', '.gif', '.png', '.gif']}
           maxFileSize={5242880}
-        /> */}
+        />
       </div>
     </div>
   );
