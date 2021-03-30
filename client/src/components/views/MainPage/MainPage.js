05be92ab07cecaccb878092a6532af36d5742636
이주이 <dea8307@naver.com>
2021-03-30 16:54:04 +0900

    feat: MainPage GPS DONE

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { Typography } from 'antd';
 import MainPageBar from './Sections/MainPageBar';
@@ -7,8 +8,12 @@ import StoreCard from 'utils/StoreCard';
 import { fetchStoresMainPage } from '_api/Stores';
 import { fetchInfluencer } from '_api/Main';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { changeUserInfo } from '../../../_actions/user_actions';
+
+const { kakao } = window;
 
 function MainPage(props) {
+  const dispatch = useDispatch();
   const user = useSelector(state => state.user);
   const [Address, setAddress] = useState(''); //초기에 입력한 주소 받아와서 할당하기
   const [Fooders, setFooders] = useState([]);
@@ -16,9 +21,31 @@ function MainPage(props) {
   const { Title, Text } = Typography;
 
   useEffect(() => {
-    let newAddress = '구미 진평동';
-    setAddress(newAddress);
+    setAddress(props.user.loginSuccess.address);
+  });
 
+  //   useEffect(() => {
+  //     var geocoder = new kakao.maps.services.Geocoder();
+  //     if (navigator.geolocation) {
+  //       navigator.geolocation.getCurrentPosition(function (position) {
+  //         setlan(position.coords.latitude); // 위도
+  //         setlng(position.coords.longitude); // 경도
+  //       });
+  //     }
+  //     var coord = new kakao.maps.LatLng(lan, lng);
+  //     geocoder.coord2Address(
+  //       coord.getLng(),
+  //       coord.getLat(),
+  //       function (result, status) {
+  //         if (status === kakao.maps.services.Status.OK) {
+  //           setAddress(result[0].address.address_name);
+  //           console.log(result);
+  //         }
+  //       },
+  //     );
+  //   }, []);
+
+  useEffect(() => {
     const MainData = async () => {
       try {
         // redux에 저장된 user id 사용하기
@@ -34,12 +61,17 @@ function MainPage(props) {
     MainData();
   }, []);
 
+  const handlerAddress = ad => {
+    setAddress(ad);
+    dispatch(changeUserInfo(ad));
+  };
+
   const renderStoreCard = Stores.map((store, index) => {
     return <StoreCard store={store} key={index} />;
   });
   return (
     <div>
-      <MainPageBar address={Address} />
+      <MainPageBar change={handlerAddress} address={Address} />
       <div>
         <Title level={4}>뜨고 있는 인기 푸더!!</Title>
         <Text strong>이 지역 인기 푸더를 팔로잉하고 소식을 받아보세요!</Text>
--- client/src/components/views/MainPage/Sections/MainPageBar.js
+++ client/src/components/views/MainPage/Sections/MainPageBar.js
@@ -2,29 +2,102 @@ import React, { useState, useEffect } from 'react';
 import { Typography } from 'antd';
 import { CompassTwoTone } from '@ant-design/icons';
 import 'antd/dist/antd.css';
+import DaumPostcode from 'react-daum-postcode';
 
-function MainPageBar(props) {
-  const { Title } = Typography;
+const { Title } = Typography;
 
+const postCodeStyle = {
+  display: 'block',
+  position: 'absolute',
+  zIndex: 2,
+  top: '10%',
+  width: '400px',
+  height: '500px',
+  padding: '7px',
+};
+const { kakao } = window;
+function MainPageBar(props, { change }) {
+  const [lan, setlan] = useState('');
+  const [lng, setlng] = useState('');
+  console.log(props.address);
+  const [Address, setAddress] = useState('');
+  const [IsPostOpen, setIsPostOpen] = useState(false);
+
+  const [isZoneCode, setIsZoneCode] = useState();
+
+  const nowPlaceHandler = () => {
+    var geocoder = new kakao.maps.services.Geocoder();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setlan(position.coords.latitude); // 위도
+        setlng(position.coords.longitude); // 경도
+      });
+    }
+    var coord = new kakao.maps.LatLng(lan, lng);
+    geocoder.coord2Address(
+      coord.getLng(),
+      coord.getLat(),
+      function (result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+          setAddress(result[0].address.address_name);
+          console.log(result);
+          props.change(result[0].address.address_name);
+        }
+      },
+    );
+  };
+
+  const codeHandler = () => {
+    setIsPostOpen(!IsPostOpen);
+  };
+
+  const handleComplete = data => {
+    let fullAddress = data.address;
+
+    // console.log(data.address);
+    // if (data.addressType === 'R') {
+    //   if (data.bname !== '') {
+    //     extraAddress += data.bname;
+    //   }
+    //   if (data.buildingName !== '') {
+    //     extraAddress +=
+    //       extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+    //   }
+    //   fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    // }
+    setIsZoneCode(data.zonecode);
+    setAddress(fullAddress);
+    setIsPostOpen(!IsPostOpen);
+    props.change(fullAddress);
+  };
   return (
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '50px',
+        height: '45px',
         width: '100%',
         backgroundColor: 'orange',
       }}
     >
-      <Title level={3} style={{ margin: '1rem 1.5rem', color: 'white' }}>
+      <Title
+        level={5}
+        style={{ margin: '1rem 1rem', color: 'white' }}
+        onClick={nowPlaceHandler}
+      >
         {props.address}
       </Title>
-      <a href={'/login'}>
-        <CompassTwoTone twoToneColor="#fa8c16" style={{ fontSize: '25px' }} />
-      </a>
+
+      <CompassTwoTone
+        twoToneColor="#fa8c16"
+        style={{ fontSize: '25px' }}
+        onClick={codeHandler}
+      />
+      {IsPostOpen && (
+        <DaumPostcode style={postCodeStyle} onComplete={handleComplete} />
+      )}
     </div>
   );
 }
-
 export default MainPageBar;
