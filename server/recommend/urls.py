941c904153851e48cd36c6c83b05ac8c8324e203
seaw7777 <72549589+seaw7777@users.noreply.github.com>
2021-04-04 13:24:11 +0900

    동행자 추천 주석제거

--- server/recommend/urls.py
+++ server/recommend/urls.py
@@ -5,7 +5,6 @@ app_name = 'recommend'
 
 urlpatterns = [
     path('recommenduser/<int:id>/', views.recommenduser),
-    path('recommendtest/',views.test),
     path('recommendstore/<int:id>/',views.recommendStore),
     path('recommendcompanion/',views.recommendcompanion)
 ]
