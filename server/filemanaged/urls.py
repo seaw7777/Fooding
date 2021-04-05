f1fd3af1b202e5cdb046ac00eb1f0762d445cbf7
YoonJeju <orange5412@naver.com>
2021-04-05 16:54:41 +0900

    fileUploadPost

--- server/filemanaged/urls.py
+++ server/filemanaged/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import fileUp
 
 app_name = 'filemanaged'
 
 urlpatterns = [
-    path('fileupload/' , views.fileupload)
+    path('fileupload/' , fileUp.as_view({"post":"fileupload"})),
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
--- server/filemanaged/views.py
+++ server/filemanaged/views.py
@@ -9,21 +9,25 @@ from rest_framework.decorators import api_view
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 import os
+from drf_yasg.utils import swagger_auto_schema
+from django.views import View
+from .serializers import uploadImage
 
 # Create your views here.
-@api_view(['POST'])
-def fileupload(request):
-    file = request.FILES.getlist('files')
-    print(request.method)
-    # print(request.FILES)
-    # user_id = request.data.get('user_id')
-    # print(user_id)
-    for i in file:
-    #     print(i)
-        # i.save('test.png',content,save=True)       
-        path = default_storage.save('./media', ContentFile(i.read()))
-        # os.renames(i , "C:/SSAFY/IMG/test.png")
-        # tmp_file = os.path.join(settings.MEDIA_ROOT, path)
-        
+class fileUp(viewsets.GenericViewSet, View):
+    @swagger_auto_schema(request_body=uploadImage)
+    def fileupload(request):
+        file = request.FILES.getlist('files')
+        print(request.method)
+        # print(request.FILES)
+        # user_id = request.data.get('user_id')
+        # print(user_id)
+        for i in file:
+        #     print(i)
+            # i.save('test.png',content,save=True)       
+            path = default_storage.save('./media', ContentFile(i.read()))
+            # os.renames(i , "C:/SSAFY/IMG/test.png")
+            # tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+            
 
-    return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
